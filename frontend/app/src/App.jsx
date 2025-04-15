901517aaa1000bc19723acc5d0ce1efc1b5deffd
Rakshith-28 <NNM23CS278@NMAMIT.IN>
2025-04-15 19:41:23 +0530

    push

---
+++ frontend/app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <h1 className="p-6 m-9 bg-yellow-200 text-blue-600">Test</h1>
+    </>
+  )
+}
+
+export default App
